3539efba3503a7d135002ec61399301921bf9d70
lhoover741 <michaeldurkins@gmail.com>
2026-05-08 09:30:17 -0500

    Add admin tools route

--- artifacts/ravishing-beaute-pwa/src/App.tsx
+++ artifacts/ravishing-beaute-pwa/src/App.tsx
@@ -10,6 +10,7 @@ import Book from "@/pages/Book";
 import About from "@/pages/About";
 import Admin from "@/pages/Admin";
 import AdminPricing from "@/pages/AdminPricing";
+import AdminTools from "@/pages/AdminTools";
 import Debug from "@/pages/Debug";
 import NotFound from "@/pages/not-found";
 
@@ -26,6 +27,7 @@ function Router() {
         <Route path="/about" component={About} />
         <Route path="/admin" component={Admin} />
         <Route path="/admin/pricing" component={AdminPricing} />
+        <Route path="/admin/tools" component={AdminTools} />
         <Route path="/debug" component={Debug} />
         <Route component={NotFound} />
       </Switch>
